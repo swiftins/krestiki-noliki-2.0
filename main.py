f6377c8e02f7718fd5abd528e6bf96053281c007
АСУС <natalja.rejenta@gmail.com>
2025-02-08 17:06:08 +0200

    ADD  NO WINNERS TO LOGIC

--- main.py
+++ main.py
@@ -8,23 +8,28 @@ window.geometry("300x350")
 current_player = "X"
 buttons = []
 
-
 def check_winner():
     for i in range(3):
-        if buttons[i][0]['text'] == buttons[i][1]['text']== buttons[i][2]['text'] != "":
+        if buttons[i][0]['text'] == buttons[i][1]['text'] == buttons[i][2]['text'] != "":
             return True
-        if buttons[0][i]['text'] == buttons[1][i]['text']== buttons[2][i]['text'] != "":
+        if buttons[0][i]['text'] == buttons[1][i]['text'] == buttons[2][i]['text'] != "":
             return True
     
-    if buttons[0][0]['text'] == buttons [1][1]['text'] == buttons [2] [2]['text'] != "" :
+    if buttons[0][0]['text'] == buttons[1][1]['text'] == buttons[2][2]['text'] != "":
         return True
-    if buttons[0][2]['text'] == buttons [1][1]['text'] == buttons [2] [0]['text'] != "" :
+    if buttons[0][2]['text'] == buttons[1][1]['text'] == buttons[2][0]['text'] != "":
         return True
     return False
 
+def check_draw():
+    for row in buttons:
+        for btn in row:
+            if btn['text'] == "":
+                return False
+    return True
 
 def on_click(row, col):
-    global current_player  # Исправлено: правильное объявление глобальной переменной
+    global current_player
 
     if buttons[row][col]['text'] != "":
         return
@@ -32,8 +37,12 @@ def on_click(row, col):
 
     if check_winner():
         messagebox.showinfo("Игра окончена", f"Игрок {current_player} победил!")
-
-    current_player = 'O' if current_player == 'X' else 'X'  # Исправлено: символ '0' заменен на 'O'
+        window.quit()
+    elif check_draw():
+        messagebox.showinfo("Игра окончена", "СЕГОДНЯ ПОБЕДИТЕЛЕЙ НЕТ")
+        window.quit()
+    else:
+        current_player = 'O' if current_player == 'X' else 'X'
 
 for i in range(3):
     row = []
@@ -44,4 +53,5 @@ for i in range(3):
     buttons.append(row)
 
 window.mainloop()
+
 
