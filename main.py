8e4085bd968fe54aafd49793d47b1c8832c2f78e
АСУС <natalja.rejenta@gmail.com>
2025-02-08 17:33:15 +0200

    color added

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from tkinter import messagebox
 window = tk.Tk()
 window.title("Крестики-нолики")
 window.geometry("300x350")
+window.configure(bg="lightblue")  # Заливка экрана голубым цветом
 
 current_player = "X"
 buttons = []
@@ -53,4 +54,4 @@ for i in range(3):
         row.append(btn)
     buttons.append(row)
 
-window.mainloop() 
+window.mainloop()
